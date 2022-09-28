e24270a8b706c8eeacf69372b90da9b037ea7b1d
PTLSHIV <114471465+PTLSHIV@users.noreply.github.com>
2022-09-28 11:19:16 +0530

    Training Assignment

---
+++ Assignment1.js
@@ -0,0 +1,6 @@
+let country="India";
+let continent="";
+let population="135cr";
+
+
+console.log("Country name is"+country+"\t it's continent is"+continent+"\t and total population of India is"+population);
